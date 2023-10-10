f1cce1abe5041ad543f8defa645c79fa7d86f35b
Zyrone <bernimalt@gmail.com>
2023-10-10 10:58:36 +0200

    Display selection list buttons mit heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Malte' },
+  { id: 13, name: 'Nele' },
+  { id: 14, name: 'Ole' },
+  { id: 15, name: 'Jonas' },
+  { id: 16, name: 'Herbert' },
+  { id: 17, name: 'Timo' },
+  { id: 18, name: 'Ella' },
+  { id: 19, name: 'Sibille' },
+  { id: 20, name: 'Tornado' }
+];
